64dee0ddab1ee1cf5e21d3b3da8ad6dd0414066f
tarosuke <felis.silvestris.catus.7777@gmail.com>
2019-10-20 15:26:45 +0900

    add badge

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Badge} from '@ionic-native/badge/ngx'
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public badge: Badge
+  ) {
+  }
 
+  upBadge(){
+    this.badge.increase(1);
+  }
+
+  clearBadge(){
+    this.badge.clear();
   }
 
 }
